d7b44643975c173608d75afd5276e347cae5d6b7
Mansur <vakhabov.mans@gmail.com>
2025-08-17 02:42:30 +0300

    upt module.app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagsModule } from './tags/tags.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from './ormConfig';
+import { config } from './typeOrmConfig';
 
 @Module({
   imports: [TypeOrmModule.forRoot(config), TagsModule],
